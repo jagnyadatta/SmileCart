732177163d65dba6613da25c0202ce60204e9b7f
jagnyadatta <jagnyadattadalai99215@gmail.com>
2025-06-12 12:42:28 +0530

    Implemented search functionality to product listing page

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -1,8 +1,15 @@
 import axios from "axios";
-import { keysToCamelCase } from "neetocist";
+import { keysToCamelCase, serializeKeysToSnakeCase } from "neetocist";
+import { evolve } from "ramda";
 
 const transformResponseKeysToCamelCase = response => {
   if (response.data) response.data = keysToCamelCase(response.data);
+};
+
+const requestInterceptors = () => {
+  axios.interceptors.request.use(
+    evolve({ data: serializeKeysToSnakeCase, params: serializeKeysToSnakeCase })
+  );
 };
 
 const responseInterceptors = () => {
@@ -25,4 +32,5 @@ export default function initializeAxios() {
     "https://smile-cart-backend-staging.neetodeployapp.com/";
   setHttpHeaders();
   responseInterceptors();
+  requestInterceptors();
 }
